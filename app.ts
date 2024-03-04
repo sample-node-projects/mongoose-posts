698ee5861da1bb7b3c9c055f04f2235ffd9dbeb4
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-03-05 01:50:41 +0530

    conditionally insert user

--- app.ts
+++ app.ts
@@ -10,3 +10,19 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000);
+mongoose
+  .connect(
+    "mongodb+srv://malhotrasaksham:8hiICmjloNiGsWxO@cluster0.tcgrofx.mongodb.net/posts?retryWrites=true&w=majority"
+  )
+  .then(async () => {
+    user = await User.findOne().exec();
+    if (!user) {
+      user = await User.create({ name: "Saksham", email: "saksham@email.com" });
+    }
+  })
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database", err);
+  });
