52f6d1187f6d1b9503a7e70876d125667a31f602
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-03-05 00:18:42 +0530

    connect to mongodb

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -9,4 +10,13 @@ app.use((req, res, next) => {
   res.json({ message: "Hi" });
 });
 
-app.listen(3000);
+mongoose
+  .connect(
+    "mongodb+srv://malhotrasaksham:8hiICmjloNiGsWxO@cluster0.tcgrofx.mongodb.net/posts?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database", err);
+  });
