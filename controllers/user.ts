afb015eacf7e1fc1d84d7eba1f453350b19804ff
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-03-05 01:49:45 +0530

    get user by id controller

--- controllers/user.ts
+++ controllers/user.ts
@@ -4,3 +4,14 @@ export async function getAllUsers(req: any, res: any) {
   const users = await User.find().exec();
   res.json({ message: "Successfully fetched users", data: users });
 }
+
+export async function getUserById(req: any, res: any) {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findById(userId).exec();
+    return res.json({ message: "User created!", data: user });
+  } catch {
+    res.status(404).json({ message: "User not found" });
+  }
+}
