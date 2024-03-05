dacb61d85bc1e9a983ec3b9909a36c308832a701
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-03-05 13:31:20 +0530

    Add types to get user by id

--- controllers/user.ts
+++ controllers/user.ts
@@ -10,13 +10,17 @@ export async function getAllUsers(
   res.json({ message: "Successfully fetched users", data: users });
 }
 
-export async function getUserById(req: any, res: any) {
+export async function getUserById(
+  req: Request<{ id: string }>,
+  res: Response<HttpResponse<IUser | null>>
+) {
   const userId = req.params.id;
 
   try {
-    const user = await User.findById(userId).exec();
-    return res.json({ message: "User created!", data: user });
-  } catch {
+    const user = await User.findById(userId).select("-__v").exec();
+    return res.json({ message: "User found!", data: user });
+  } catch (err) {
+    console.log(err);
     res.status(404).json({ message: "User not found" });
   }
 }
