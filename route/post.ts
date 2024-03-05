acd2adbd84741aa10dee038d56c62d1acf7879e9
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-03-05 17:11:33 +0530

    reorder routes

--- route/post.ts
+++ route/post.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { addPost, getPostByID, getPosts } from "../controllers/post";
 
 const postRouter = Router();
 
-postRouter.get("/:id", getPostByID);
-
 postRouter.get("/all", getPosts);
+
+postRouter.get("/:id", getPostByID);
 
 postRouter.post("/add", addPost);
 
