3122ec6e9dcf8535e366911c96b0dda8317c2ba8
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-03-05 17:10:56 +0530

    add post controller

--- controllers/post.ts
+++ controllers/post.ts
@@ -1,5 +1,21 @@
-function addPost() {
+import { Request, Response } from "express";
+import { IPost, Post } from "../model/post";
+import { HttpResponse } from "../types/http-response";
 
+export async function addPost(
+  req: Request<{}, {}, IPost & { userId: string }>,
+  res: Response<HttpResponse<IPost | null>>
+) {
+  const post = new Post(req.body);
+  const savedPost = await post.save();
+  res.json({
+    message: "Post added successfully!",
+    data: {
+      _id: savedPost._id,
+      title: savedPost.title,
+      description: savedPost.description,
+    } as IPost,
+  });
 }
 
 function getPostByID() {
